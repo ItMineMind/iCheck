0b4ce18f0a5e4e2ea849d9bfb8b0963b86ac668f
suttha jongjaroen <6410615121@cn330>
2024-06-05 15:21:29 +0700

    blank login page

---
+++ icheck/src/app/login/page.js
@@ -0,0 +1,29 @@
+'use client'
+import { useState } from "react";
+export default function Page(){
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const handleEmailChange = (e) => {
+        console.log("changed email")
+        setEmail(e.target.value);
+    };
+
+    const handlePasswordChange = (e) => {
+        console.log("changed password")
+        setPassword(e.target.value);
+    };
+
+    const handleSubmit = () => {
+
+    }
+
+
+    return(
+        <>
+            email<input type='text' onChange={handleEmailChange}/><br />
+            password<input type='password' onChange={handlePasswordChange}/><br />
+            <input type='submit' onClick={handleSubmit}/>
+        </>
+    );
+}
