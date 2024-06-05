fa26e88375d04273611f11c479c495d4caf4eb52
suttha jongjaroen <6410615121@cn330>
2024-06-05 13:26:03 +0700

    crud

--- icheck/api/firebaseCRUD.js
+++ icheck/api/firebaseCRUD.js
@@ -1,5 +1,5 @@
 import { firestore } from "./firebaseConfig";
-import { addDoc, collection, getDocs, doc, setDoc } from "firebase/firestore";
+import { addDoc, collection, getDocs, doc, setDoc, deleteDoc } from "firebase/firestore";
 
 const addItem = async (title, isAllow, content, imgPath) => {
     const docRef = await addDoc(collection(firestore, "items"), {
@@ -13,7 +13,19 @@ const addItem = async (title, isAllow, content, imgPath) => {
 }
 
 const deleteItem = async (id) => {
-    
+    await deleteDoc(doc(db, "items", id));
+}
+
+const getItems = async() => {
+    const itemDocs = await getDocs(collection(firestore, "items"));
+    const itemsData = courseDocs.docs.map((doc) => doc.data());
+
+    return itemsData;
+}
+
+const getItem = async(id) => {
+    const items = await getDocs
+
 }
 
 
